6b89780af1e6c4202fdd7c75d11da279488dffd8
PavelSteep <141700339+PavelSteep@users.noreply.github.com>
2024-09-24 18:20:51 +0200

    mod-1.js(task-3)

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 function getElementWidth(content, padding, border) {
     const contentWidth = Number.parseFloat(content);
-    const paddingWidth = Number.parseFloat(padding);
-    const borderWidth = Number.parseFloat(border);
+    const paddingWidth = Number.parseFloat(padding) * 2;
+    const borderWidth = Number.parseFloat(border) * 2;
   return contentWidth + paddingWidth + borderWidth;
 }
 
